4e9f132035752aeea68333873ed83273176428f5
dvdmarveira <ddmdoliveira@gmail.com>
2025-03-24 16:36:59 -0300

    chore: configure node env

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');  // Importa o middleware cors
-const complaintsRoute = require('./routes/complaints');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors"); // Importa o middleware cors
+const complaintsRoute = require("./routes/complaints");
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());  // Ativa o CORS para todas as rotas
+app.use(cors()); // Ativa o CORS para todas as rotas
 
 // Caso fosse limitar o acesso do CORS
 // const corsOptions = {
@@ -19,9 +19,19 @@ app.use(cors());  // Ativa o CORS para todas as rotas
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI);
+// Verifica se está em produção ou desenvolvimento
+const dbURI =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGODB_URI // Produção (MongoDB Atlas)
+    : "mongodb://localhost:27017/app_demo"; // Desenvolvimento (MongoDB local)
 
-app.use('/api/complaints', complaintsRoute);
+// Conectar ao MongoDB com a URI correspondente
+mongoose
+  .connect(dbURI)
+  .then(() => console.log("Conectado ao MongoDB"))
+  .catch((err) => console.error("Erro ao conectar ao MongoDB:", err));
+
+app.use("/api/complaints", complaintsRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
